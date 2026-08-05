59967bc28fa8ec42a96ead512ba3abc02e55adee
Riyane Bouakaz <riyane.bouakaz@etu.cyu.fr>
2026-08-05 20:31:10 +0200

    Ajout commentaire

--- traitement.py
+++ traitement.py
@@ -2,7 +2,10 @@ from llama_index.core import SimpleDirectoryReader
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.readers.file import PyMuPDFReader
 
+#On choisis l'outil pour extraire le texte des fichiers PDF
 extracteur = {"pdf": PyMuPDFReader()}
+
+#On charge les documents depuis le dossier "data" en utilisant l'extracteur
 documents = SimpleDirectoryReader("data", file_extractor=extracteur).load_data()
 for doc in documents:
     print("\n----------------------------")
@@ -11,6 +14,7 @@ for doc in documents:
 
 print(f"Nombres de documents chargés: {len(documents)}")
 
+#On découpe les documents en chunks de 1000 tokens avec un chevauchement de 200 tokens
 splitter = SentenceSplitter(chunk_size=1000, chunk_overlap=200)
 chunk = splitter.get_nodes_from_documents(documents)
 
