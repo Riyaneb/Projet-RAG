24bc5703062ae4a10a96c6a046a49f767dfec51e
Riyane Bouakaz <riyane.bouakaz@etu.cyu.fr>
2026-08-05 20:30:08 +0200

    Problème chunk resolu

--- traitement.py
+++ traitement.py
@@ -1,7 +1,9 @@
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core.node_parser import SentenceSplitter
+from llama_index.readers.file import PyMuPDFReader
 
-documents = SimpleDirectoryReader("data").load_data()
+extracteur = {"pdf": PyMuPDFReader()}
+documents = SimpleDirectoryReader("data", file_extractor=extracteur).load_data()
 for doc in documents:
     print("\n----------------------------")
     print(doc.metadata)
@@ -13,3 +15,5 @@ splitter = SentenceSplitter(chunk_size=1000, chunk_overlap=200)
 chunk = splitter.get_nodes_from_documents(documents)
 
 print(f"Nombres de chunks créés: {len(chunk)}")
+print("Test premier chunk")
+print(chunk[0].text)
