d3b4291f7d85372786e464eeabff484352e851c1
Riyane Bouakaz <riyane.bouakaz@etu.cyu.fr>
2026-08-05 20:02:03 +0200

    Extraction des documents mais y'a un problème avec la génération des chunks

---
+++ traitement.py
@@ -0,0 +1,15 @@
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core.node_parser import SentenceSplitter
+
+documents = SimpleDirectoryReader("data").load_data()
+for doc in documents:
+    print("\n----------------------------")
+    print(doc.metadata)
+    print("----------------------------\n")
+
+print(f"Nombres de documents chargés: {len(documents)}")
+
+splitter = SentenceSplitter(chunk_size=1000, chunk_overlap=200)
+chunk = splitter.get_nodes_from_documents(documents)
+
+print(f"Nombres de chunks créés: {len(chunk)}")
